d607fb74b3821afbefe67083ae2034d972bf562a
David Mueller <quanterium@users.noreply.github.com>
2016-01-16 23:13:11 -0800

    version 1.4.1 with updated signature certificates

--- LongURL for Safari.safariextension/LongURL4Safari.js
+++ LongURL for Safari.safariextension/LongURL4Safari.js
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2010-2015, David Mueller, TiTi
+Copyright (c) 2010-2016, David Mueller, TiTi
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
